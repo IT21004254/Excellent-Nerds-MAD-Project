93780c0154a0cc58252e30813d76a061a0ceaaf4
IT21004254 <87602692+IT21004254@users.noreply.github.com>
2022-10-23 15:24:01 +0530

    Himash Part

--- app/src/main/java/com/example/assignment/ECGTest.java
+++ app/src/main/java/com/example/assignment/ECGTest.java
@@ -6,9 +6,11 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,8 +19,9 @@ public class ECGTest extends AppCompatActivity {
 
     TextView getdate,gettime,getlocation;
     String datepass,timepass, locationpass;
-    private Button cancelbooking, gotohome;
-    Dialog dialog, dialog1;
+    private Button cancelbooking, gotohome, pay;
+    Dialog dialog, dialog1, dialog2;
+    EditText name,age;
 
 
     @Override
@@ -126,6 +129,62 @@ public class ECGTest extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dialog1.dismiss();
+            }
+        });
+
+//Pay Button//
+        pay = findViewById(R.id.pay);
+
+        name = findViewById(R.id.PersonName);
+        age = findViewById(R.id.ageECG);
+
+        dialog2 = new Dialog(ECGTest.this);
+        dialog2.setContentView(R.layout.custombackgroundpaynow);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            dialog2.getWindow().setBackgroundDrawable(getDrawable(R.drawable.background));
+        }
+        dialog2.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        dialog2.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+
+        Button okaypay = dialog2.findViewById(R.id.btn_okay_pay);
+        Button cancelpay = dialog2.findViewById(R.id.btn_cancel_pay);
+        dialog2.setCancelable(false);
+
+
+        okaypay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (ECGTest.this,WelcomePage.class);
+                startActivity(intent);
+            }
+        });
+
+        cancelpay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog2.dismiss();
+            }
+        });
+
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(name.getText().toString()))
+                {
+                    Toast.makeText(ECGTest.this,"Please Enter Patient Name", Toast.LENGTH_LONG).show();
+                }
+
+                else if (TextUtils.isEmpty(age.getText().toString()))
+                {
+                    Toast.makeText(ECGTest.this,"Please Enter Patient Age", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    dialog1.show();
+
+                }
             }
         });
     }
